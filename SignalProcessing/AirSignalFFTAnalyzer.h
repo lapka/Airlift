4cf67582399aece5b42dbf2387955c29958ecdd9
Sergey Filippov <duhovny@gmail.com>
2013-09-23 11:55:14 +0400

    Update FFT Analyzer with array of required frequencies

--- SignalProcessing/AirSignalFFTAnalyzer.h
+++ SignalProcessing/AirSignalFFTAnalyzer.h
@@ -11,20 +11,21 @@
 
 @interface AirSignalFFTAnalyzer : NSObject {
 	
-	COMPLEX_SPLIT   fft_complex_split;
-    FFTSetup        fft_setup;
-    uint32_t        log2n;
-    uint32_t        n, nOver2;
-    int32_t         stride;
-    Float32        *obtainedReal;
-	int32_t        *obtained_int;
-    Float32         scale;
+	COMPLEX_SPLIT   _fft_complex_split;
+    FFTSetup        _fft_setup;
+    uint32_t        _log2n;
+    uint32_t        _n, _nOver2;
+    int32_t         _stride;
+    Float32        *_obtainedReal;
+	int32_t        *_obtained_int;
+    Float32         _scale;
+	Float32			_sampleRate;
+	uint32_t	   *_required_bins;
+	uint32_t		_required_bins_count;
+	Float32		   *_amplitudes;
 }
 
-@property (nonatomic, assign) double frequency;
-@property (nonatomic, assign) double sampleRate;
-
-- (id)initWithNumberOfFrames:(UInt32)numberOfFrames;
-- (Float32)processFFTWithData:(Float32 *)data;
+- (id)initWithNumberOfFrames:(UInt32)numberOfFrames sampleRate:(Float32)sampleRate requiredFrequencies:(Float32 *)requiredFrequencies requiredFrequenciesCount:(int)requiredFrequenciesCount;
+- (Float32 *)processFFTWithData:(Float32 *)data;
 
 @end
--- SignalProcessing/AirSignalFFTAnalyzer.m
+++ SignalProcessing/AirSignalFFTAnalyzer.m
@@ -7,46 +7,58 @@
 
 @implementation AirSignalFFTAnalyzer
 
-@synthesize frequency;
-@synthesize sampleRate;
-
 
 #pragma mark -
 #pragma mark Lifecycle
 
 
-- (id)initWithNumberOfFrames:(UInt32)numberOfFrames {
+- (id)initWithNumberOfFrames:(UInt32)numberOfFrames
+				  sampleRate:(Float32)sampleRate
+		 requiredFrequencies:(Float32 *)requiredFrequencies
+	requiredFrequenciesCount:(int)requiredFrequenciesCount
+{
 	if ((self = [super init])) {
 		
 		// Set the size of FFT.
-		n = numberOfFrames;
-		log2n = log2(n);
-		stride = 1;
-		nOver2 = n / 2;
+		_sampleRate = sampleRate;
+		_n = numberOfFrames;
+		_log2n = log2(_n);
+		_stride = 1;
+		_nOver2 = _n / 2;
 		
 		NSLog(@"---");
 		NSLog(@"FFT Analyzer Init");
-		NSLog(@"n: %d", n);
-		NSLog(@"log2n: %d", log2n);
-		NSLog(@"nOver2: %d", nOver2);
+		NSLog(@"n: %d", _n);
+		NSLog(@"log2n: %d", _log2n);
+		NSLog(@"nOver2: %d", _nOver2);
 		NSLog(@"---");
 		
 		// Allocate memory for the input operands and check its availability,
 		// use the vector version to get 16-byte alignment.
-		fft_complex_split.realp = (Float32 *) malloc(nOver2 * sizeof(Float32));
-		fft_complex_split.imagp = (Float32 *) malloc(nOver2 * sizeof(Float32));
-		obtainedReal = (Float32 *) malloc(n * sizeof(Float32));
-		obtained_int = (int32_t *) malloc(n * sizeof(int32_t));
+		_fft_complex_split.realp = (Float32 *) malloc(_nOver2 * sizeof(Float32));
+		_fft_complex_split.imagp = (Float32 *) malloc(_nOver2 * sizeof(Float32));
+		_obtainedReal = (Float32 *) malloc(_n * sizeof(Float32));
+		_obtained_int = (int32_t *) malloc(_n * sizeof(int32_t));
 		
-		if (fft_complex_split.realp == NULL || fft_complex_split.imagp == NULL) {
+		if (_fft_complex_split.realp == NULL || _fft_complex_split.imagp == NULL) {
 			printf("SSignalFFTAnalyzer: malloc failed to allocate memory for the real FFT section of the sample.\n");
 		}
 		
 		// Set up the required memory for the FFT routines and check its availability.
-		fft_setup = vDSP_create_fftsetup(log2n, FFT_RADIX2);
-		if (fft_setup == NULL) {
+		_fft_setup = vDSP_create_fftsetup(_log2n, FFT_RADIX2);
+		if (_fft_setup == NULL) {
 			printf("SSignalFFTAnalyzer: FFT_Setup failed to allocate enough memory for the real FFT.\n");
 		}
+		
+		// Calculate required bins
+		_required_bins_count = requiredFrequenciesCount;
+		_required_bins = (uint32_t *) malloc(_required_bins_count * sizeof(uint32_t));
+		for (int i = 0; i < _required_bins_count; i++) {
+			_required_bins[i] = roundf(requiredFrequencies[i] * _n / _sampleRate);
+		}
+		
+		// Create amplitudes array
+		_amplitudes = (Float32 *) malloc(_required_bins_count * sizeof(Float32));
 	}
 	return self;
 }
@@ -55,11 +67,13 @@
 	
 	/* Free the allocated memory. */
 	
-    vDSP_destroy_fftsetup(fft_setup);
-    free(obtainedReal);
-	free(obtained_int);
-    free(fft_complex_split.realp);
-    free(fft_complex_split.imagp);
+    vDSP_destroy_fftsetup(_fft_setup);
+    free(_obtainedReal);
+	free(_obtained_int);
+    free(_fft_complex_split.realp);
+    free(_fft_complex_split.imagp);
+	free(_required_bins);
+	free(_amplitudes);
 }
 
 
@@ -67,41 +81,40 @@
 #pragma mark Proccess FFT
 
 
-- (Float32)processFFTWithData:(Float32 *)data {
+- (Float32 *)processFFTWithData:(Float32 *)data {
 	
 	/* Look at the real signal as an interleaved complex vector by
      * casting it. Then call the transformation function vDSP_ctoz to
      * get a split complex vector, which for a real signal, divides into
      * an even-odd configuration. */
 	
-    vDSP_ctoz((COMPLEX *)data, 2, &fft_complex_split, 1, nOver2);
+    vDSP_ctoz((COMPLEX *)data, 2, &_fft_complex_split, 1, _nOver2);
 	
 	/* Carry out a Forward FFT transform. */
 	
-    vDSP_fft_zrip(fft_setup, &fft_complex_split, stride, log2n, FFT_FORWARD);
+    vDSP_fft_zrip(_fft_setup, &_fft_complex_split, _stride, _log2n, FFT_FORWARD);
 	
 	/* Verify correctness of the results, but first scale it by  2n. */
 	
-    scale = (Float32) 1.0 / (2 * n);
-    vDSP_vsmul(fft_complex_split.realp, 1, &scale, fft_complex_split.realp, 1, nOver2);
-    vDSP_vsmul(fft_complex_split.imagp, 1, &scale, fft_complex_split.imagp, 1, nOver2);
+    _scale = (Float32) 1.0 / (2 * _n);
+    vDSP_vsmul(_fft_complex_split.realp, 1, &_scale, _fft_complex_split.realp, 1, _nOver2);
+    vDSP_vsmul(_fft_complex_split.imagp, 1, &_scale, _fft_complex_split.imagp, 1, _nOver2);
 	
 	/* The output signal is now in a split real form. Use the function
      * vDSP_ztoc to get a split real vector. */
 	
-    vDSP_ztoc(&fft_complex_split, 1, (COMPLEX *)obtainedReal, 2, nOver2);
+    vDSP_ztoc(&_fft_complex_split, 1, (COMPLEX *)_obtainedReal, 2, _nOver2);
 	
-	/* Find amplitude by frequency */
+	/* Find amplitudes by required bins */
 	
-	UInt32 required_bin = roundf(frequency * n / sampleRate);
-	
-	/* FFT Parts */
-	
-	double requiredReal = fft_complex_split.realp[required_bin];
-	double requiredImag = fft_complex_split.imagp[required_bin];
-	
-	Float32 amplitude = sqrtf(requiredReal * requiredReal + requiredImag * requiredImag);
-	return amplitude;
+	for (int i = 0; i < _required_bins_count; i++) {
+		uint32_t required_bin = required_bins[i];
+		double requiredReal = _fft_complex_split.realp[required_bin];
+		double requiredImag = _fft_complex_split.imagp[required_bin];
+		_amplitudes[i] = sqrtf(requiredReal * requiredReal + requiredImag * requiredImag);
+	}
+
+	return _amplitudes;
 }
 
 @end
